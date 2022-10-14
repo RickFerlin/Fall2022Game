b62a1130fbff2bd03ecfc29fbb58c2e50bc62d29
Rick <10963428@uvu.edu>
2022-10-13 20:59:27 -0600

    StartedProjectDrHackr

---
+++ DrHackr/Assets/Scripts/ColorMatchBehaviour.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ColorMatchBehaviour : MatchBehaviour
+{
+    public ColorIDDataList colorIDDataListObj;
+
+    private void Awake()
+    {
+        idObj = colorIDDataListObj.currentColor;
+    }
+
+    public void ChangeColor(SpriteRenderer renderer)
+    {
+        var newColor = idObj as ColorID;
+        renderer.color = newColor.value;
+    }
+}
